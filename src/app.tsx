dd2f74fab3ac5dca74c3b9921c3e7c5fb8baaf7f
ricsjs <ricfilho0007@gmail.com>
2024-08-08 16:54:09 -0300

    feat: create pages

---
+++ src/app.tsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { CreateRoom } from "./pages/create-room";
+import { Room } from "./pages/room";
+import { Toaster } from "sonner";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <CreateRoom />,
+  },
+  {
+    path: "/room/:roomId",
+    element: <Room />,
+  },
+]);
+
+export function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+      <Toaster invert richColors/>
+    </>
+  );
+}
